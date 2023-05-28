f6b4584a5d543badddf76fe7070da0335c6162b4
pgdureza <pgdureza@gmail.com>
2023-05-28 18:31:57 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: "src/components/index.ts",
+      name: "ub-lc-component-library",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
   plugins: [react()],
-})
+});
